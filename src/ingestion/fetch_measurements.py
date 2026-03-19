360337aafd9d369cfe562d7fcae3123790ad8b35
cmin <chong.m2007@gmail.com>
2026-03-18 20:04:06 -0700

    add paginated groundwater ingestion

--- src/ingestion/fetch_measurements.py
+++ src/ingestion/fetch_measurements.py
@@ -4,31 +4,27 @@ from pathlib import Path
 
 
 DB_PATH = Path(__file__).resolve().parents[2] / "groundwater.db"
-url = 'https://data.cnra.ca.gov/api/3/action/datastore_search?resource_id=bfa9f262-24a1-45bd-8dc8-138bc8107266&limit=5'  
-response = requests.get(url)
-data = response.json()["result"]["records"]
 
-print("Writing to:", DB_PATH)
-print("Records pulled:", len(data))
-
-conn = sqlite3.connect("groundwater.db")
+conn = sqlite3.connect(DB_PATH)
 cur = conn.cursor()
 
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS measurements (
-        site_code TEXT,
-        msmt_date TEXT,
-        gwe REAL,
-        wlm_qa_desc TEXT,
-        UNIQUE(site_code, msmt_date)
-    )
-    """)
+OFFSET_LIMIT = 1000
+offset = 0
+while True:
+    print(offset)
+    url = f'https://data.cnra.ca.gov/api/3/action/datastore_search?resource_id=bfa9f262-24a1-45bd-8dc8-138bc8107266&limit={OFFSET_LIMIT}&offset={offset}'
+    response = requests.get(url)
+    data = response.json()["result"]["records"]
+    batch = [(d["site_code"], d["msmt_date"], d["gwe"], d["wlm_qa_desc"]) for d in data]
+    if len(data) == 0:
+        break
 
-for row in data:
-    cur.execute("""
+    cur.executemany("""
         INSERT OR IGNORE INTO measurements (site_code, msmt_date, gwe, wlm_qa_desc) 
         VALUES (?, ?, ?, ?)
-    """, (row["site_code"], row["msmt_date"], row["gwe"], row["wlm_qa_desc"]))
+    """, batch)
+    offset += OFFSET_LIMIT
+
 
 conn.commit()
 conn.close()
