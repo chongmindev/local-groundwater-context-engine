e6135ba5a727adb848b3734d0f937a7a66a4c07d
cmin <chong.m2007@gmail.com>
2026-04-02 21:10:10 -0700

    Add incremental ingestion with metadata checkpoint

--- src/ingestion/ingest_measurements.py
+++ src/ingestion/ingest_measurements.py
@@ -1,6 +1,7 @@
 import requests
 import sqlite3
 from pathlib import Path
+from urllib.parse import quote_plus
 
 DB_PATH = Path(__file__).resolve().parents[2] / "groundwater.db"
 
@@ -10,9 +11,15 @@ def ingest_measurements():
 
     OFFSET_LIMIT = 10000
     offset = 0
+    latest_date = cur.execute("""
+            SELECT value FROM metadata WHERE key = 'latest_date'
+        """).fetchone()
+    if latest_date is None:
+        latest_date = ('1888-01-01',)
     while True:
         print(offset)
-        url = f'https://data.cnra.ca.gov/api/3/action/datastore_search?resource_id=bfa9f262-24a1-45bd-8dc8-138bc8107266&limit={OFFSET_LIMIT}&offset={offset}'
+        sql = f"SELECT * FROM \"bfa9f262-24a1-45bd-8dc8-138bc8107266\" WHERE msmt_date > '{latest_date[0]}' LIMIT {OFFSET_LIMIT} OFFSET {offset}"
+        url = f"https://data.cnra.ca.gov/api/3/action/datastore_search_sql?sql={quote_plus(sql)}"
         response = requests.get(url)
         data = response.json()["result"]["records"]
         batch = [(d["site_code"], d["msmt_date"], d["gwe"], d["wlm_qa_desc"]) for d in data]
@@ -20,10 +27,20 @@ def ingest_measurements():
             break
 
         cur.executemany("""
-            INSERT OR IGNORE INTO measurements (site_code, msmt_date, gwe, wlm_qa_desc) 
+            INSERT INTO measurements (site_code, msmt_date, gwe, wlm_qa_desc) 
             VALUES (?, ?, ?, ?)
+            ON CONFLICT (site_code, msmt_date) 
+            DO NOTHING
         """, batch)
         offset += OFFSET_LIMIT
+    cur.execute("""
+        SELECT MAX(msmt_date) FROM measurements
+    """)
+
+    cur.execute("""
+        INSERT OR REPLACE INTO metadata (key, value) 
+        VALUES ('latest_date', ?)
+    """, (cur.fetchone()[0],))
 
     conn.commit()
     conn.close()
